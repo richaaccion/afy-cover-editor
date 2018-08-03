abb197391d9ac9666eb60d5846d170b8cfa8de18
richaaccion <richa.joshi@accionlabs.com>
2018-08-03 15:06:24 +0530

    hbr to svg conversion implemented w/o api call

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+module.exports  = function(app) {
+	initRoutes(app);
+}
+
+function initRoutes(app) {
+	app.get('/', function(req, res){
+		res.send("working here");
+	});
+}
--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-'use strict'
+var fs = require('fs');
+var path = require('path');
 
-const express = require('express');
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+var express = require('express');
 
-const app = express();
+var app = express();
+app.use('/static', express.static(path.join(__dirname, 'assets')))
 
-app.get('/', function(req, res){
-    res.send('Hello world');
+var hbrFilePath = "assets/template";
+
+var templateFileName = "template1.hbr";
+
+var fileContentBuffer = fs.readFileSync(path.join(__dirname, hbrFilePath, templateFileName));
+
+var fileContent = fileContentBuffer.toString();
+
+var replacedText = fileContent.replace("{{back_profile_pic}}", "http://localhost:3002/static/template/john-profile-photo.jpg").replace("{{front_profile_pic}}", "http://localhost:3002/static/template/john-profile-photo.jpg").replace("{{front_book_title}}", "Selling").replace("{{back_book_title}}", "Selling").replace("{{background_image}}", "http://localhost:3002/static/template/background-image.png").replace("{{author_name}}", "John Doe");
+
+app.listen(3002);
+
+app.get('/', (req, res) => {
+	res.send(replacedText);
 });
-
-app.listen(PORT, HOST);
-
-
-console.log("server running on " + PORT + HOST);
